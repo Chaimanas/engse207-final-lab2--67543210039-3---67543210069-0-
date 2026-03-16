77fb32f487ca0642719368806b491e4252832304
PhacharaChangyouang <potbunpal@gmail.com>
2026-03-16 23:01:44 +0700

    fix raiway db

--- auth-service/src/index.js
+++ auth-service/src/index.js
@@ -8,5 +8,9 @@ app.use(cors());
 
 app.use('/api/auth', authRoutes);
 
+app.get('/', (req,res)=>{
+  res.send("AUTH SERVICE OK");
+});
+
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Auth Service running on port ${PORT}`));
