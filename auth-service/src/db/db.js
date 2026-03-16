b78ded64674232c1d0f4a8f5bd6932e8c8205cd4
PhacharaChangyouang <potbunpal@gmail.com>
2026-03-16 23:01:44 +0700

    fix raiway db

--- auth-service/src/db/db.js
+++ auth-service/src/db/db.js
@@ -1,12 +1,10 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  // เปลี่ยนเป็น auth-db เพื่อให้คุยกับ container ฐานข้อมูลของ auth ได้
-  host:     process.env.DB_HOST || 'auth-db', 
-  port:     5432,
-  database: process.env.DB_DATABASE || 'authdb',  // เช็คชื่อ DB ใน docker-compose อีกทีนะ
-  user:     process.env.DB_USER || 'admin',
-  password: process.env.DB_PASSWORD || 'secret',  // เช็ค password ใน docker-compose อีกทีนะ
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = { pool };
