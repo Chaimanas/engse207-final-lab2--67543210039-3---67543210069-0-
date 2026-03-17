2c0aac425482e493a4acbcc1992da7feb168f206
PhacharaChangyouang <potbunpal@gmail.com>
2026-03-18 01:35:51 +0700

    fix admin role 2

--- set2/auth-service/src/routes/auth.js
+++ set2/auth-service/src/routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-
+const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const pool = require('../db/db');  
 const { generateToken } = require('../middleware/jwtUtils');
