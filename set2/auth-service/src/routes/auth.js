8d845410733d7f053445cb212d10d2e111f6621f
PhacharaChangyouang <potbunpal@gmail.com>
2026-03-17 21:42:05 +0700

    add bcrypvdcst

--- set2/auth-service/src/routes/auth.js
+++ set2/auth-service/src/routes/auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const bcrypt = require('bcryptls');
+const bcrypt = require('bcryptjs');
 const pool = require('../config/db'); 
 const { generateToken } = require('../utils/jwt'); 
 
