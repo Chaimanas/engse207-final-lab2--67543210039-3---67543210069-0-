5d31fa07d3d2183210b4360d8891c1adcf917c3e
PhacharaChangyouang <potbunpal@gmail.com>
2026-03-17 23:47:31 +0700

    fix: add pg dependey

---
+++ set2/frontend/config.js
@@ -0,0 +1,5 @@
+window.APP_CONFIG = {
+  AUTH_URL: 'https://set2-production.up.railway.app',
+  TASK_URL: 'https://task-service-production-073d.up.railway.app',
+  USER_URL: 'https://user-service-production-64f6.up.railway.app'
+};
