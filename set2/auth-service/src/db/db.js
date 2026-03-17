904cc1c24f6a5129834e57c8d32bab40f3e03c92
PhacharaChangyouang <potbunpal@gmail.com>
2026-03-17 18:46:48 +0700

    fix again 153

--- set2/auth-service/src/db/db.js
+++ set2/auth-service/src/db/db.js
@@ -1,5 +1,5 @@
 const { Pool } = require('pg');
-
+console.log("USING DATABASE_URL:", process.env.DATABASE_URL ? "YES" : "NO");
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
